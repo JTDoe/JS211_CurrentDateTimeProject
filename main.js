1c6998d9f6a33d1dc3898b6c67441816cc450aa4
Jtdoe <Jordendoerflinger@gmail.com>
2022-04-07 08:35:08 -0500

    first commit

--- main.js
+++ main.js
@@ -8,19 +8,27 @@
 console.log(new Date)
 
 const displayDate = () => {
-  const currentDate = new Date()
+const currentDate = new Date()
 
-  document.getElementById("display-element").innerHTML = currentDate;
+document.getElementById("display-element").innerHTML = currentDate;
 }
  
 
 // Write a JavaScript program to convert a number to a string.
+// let num = 6;
+// let text = num.toString();
+// console.log(text)
 
+// const convertNumberToString = (string) => {
+//   const result = Number(string)
+//   return result
+// }
 
+// console.log(convertNumberToString(5))
 
 // Write a JavaScript program to convert a string to the number.
-
-
+// let x = number("1000")
+// console.log(number)
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -30,21 +38,71 @@ const displayDate = () => {
   // * NaN
   // * String
   
+const checkDataType = () => {
+return typeof element
 
+}
+
+console.log(checkDataType(5))
   
 // Write a JavaScript program that adds 2 numbers together.
+let z = document.getElementById("z")
+let y = document.getElementById("y")
+let sum = z + y
 
+const addTwoNumbers = (z, y) => {
+ const sum = z + y
+ document.getElementById("sum").innerHTML
+}
+
+console.log(sum)
+
+// const num1 = 6;
+// const num2 = 5;
+
+// const sum = num1 + num2;
+
+// console.log()
 
 
 // Write a JavaScript program that runs only when 2 things are true.
+const age = 34
+const hasKids = true
+
+if (age >= 30 && hasKids) {
+  console.log("Jorden is over 30 and has kids")
+}
+
+
+
 
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
+const brownHair = false
+const blueEyes = true
+
+if (brownHair || blueEyes) {
+  console.log("jorden has brown hair or blue eyes")
+}
+
+else {
+console.log("jorden doesnt have brown hair or blue eyes")
+}
 
 
 
 // Write a JavaScript program that runs when both things are not true.  
+// const bigNose = false
+// const bigEars = false
+
+// if (bigNose || bigEars) {
+
+// }
+
+// else () {
+//     console.log("jorden doesnt have a big nose or big ears");
+//   } 
 
 // ***************************
 //         PART TWO
